d6c723fd5af4fee2157220178efec3f54098afd6
Materone <materone@gmail.com>
2014-09-17 08:05:17 +0800

    A
    A

--- ESPWifiATSoft56/ESPWifiATSoft56.ino
+++ ESPWifiATSoft56/ESPWifiATSoft56.ino
@@ -28,13 +28,22 @@ void setup() {
   SeeedOled.setHorizontalMode();      //Set addressing mode to Horizontal Mode
   SeeedOled.setTextXY(0, 0);
   SeeedOled.putString("WiFi Test\n");
+  Serial.println("AT");
+  delay(100);
   Serial.println("AT+CWJAP=\"CoolDog\",\"86053436\"");
+  getWifiInfo();
+  delay(5000);
   update1();
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
+  getWifiInfo();
+  getCmd();
+}
+
+void getWifiInfo(){
   if (Serial.available()) {
     data = "";
     cnt = 0;
@@ -57,6 +66,9 @@ void loop() {
     sPort.println(data);
     sPort.println(tCnt);
   }
+}
+
+void getCmd(){
   if (sPort.available()) {
     data = "";
     flag = true;
@@ -78,7 +90,6 @@ void loop() {
     sPort.println(data);
   }
 }
-
 void update1() {
   /*
   Serial.println("AT+CIPCLOSE");
@@ -86,10 +97,20 @@ void update1() {
   */
   sPort.print("In AT Cmd:");
   Serial.println("AT+CIPMUX=1");
+  delay(2000);
+  getWifiInfo();
   Serial.println("AT+CIPSTART=4,\"TCP\",\"materonep001.sinaapp.com\",80");
+  delay(2000);
+  getWifiInfo();
   Serial.println("AT+CIPSEND=4,77");
-  delay(500);
+  delay(2000);
+  getWifiInfo();
   Serial.print("GET /homestatus.php?h=9888&t=8889 HTTP/1.1\r\n");
+  delay(1000);
+  getWifiInfo();
   Serial.print("host:materonep001.sinaapp.com\r\n");
+  delay(1000);
+  getWifiInfo();
   Serial.print("\r\n");
+  getWifiInfo();
 }
